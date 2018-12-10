f4ab12562cf90fca81be4b17e0957ac8621ec325
noimin0610 <mii.informationsystems@gmail.com>
2018-12-11 00:39:58 +0900

    #2 カードを消したぶんの線の座標ズレを追従して直す

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -210,6 +210,16 @@ export default {
         this.cards[category][i].index = i
       }
       this.lines = this.lines.filter(e => !(e.x1 === 200 * category + 170 && e.y1 === 50 * (index + 1) + 24) && !(e.x2 === 200 * category + 30 && e.y2 === 50 * (index + 1) + 24))
+
+      for(let i in this.lines) {
+        let e = this.lines[i]
+        if (e.x1 === 200 * category + 170 && e.y1 > 50 * (index + 1) + 24) {
+          this.lines[i].y1 -= 50
+        }
+        if (e.x2 === 200 * category + 30 && e.y2 > 50 * (index + 1) + 24) {
+          this.lines[i].y2 -= 50
+        }
+      }
     }
   }
 }
