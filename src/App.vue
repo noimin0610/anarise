bc4fdb218b2b0d5cd66762853c491288d395b49e
noimin0610 <mii.informationsystems@gmail.com>
2018-12-11 19:48:07 +0900

    使い方追加

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,15 @@
   <div id="app">
     <h1>Anarise</h1>
     <router-view/>
+    <h2>Anarise のつかいかた</h2>
+    <p>3つのカテゴリにカードを追加して，カードを繋げて，自己分析を助けるツールです</p>
+    <ol>
+      <li>"+"ボタンでそのカテゴリにカードを追加</li>
+      <li>カード左上の"x"ボタンでカードを削除</li>
+      <li>カード右上の"edit"ボタンでカードのテキストを編集</li>
+      <li>2枚のカードを選択するとカードの間に線を追加</li>
+      <li>カード間の線をクリックすると線を削除</li>
+    </ol>
   </div>
 </template>
 
@@ -22,5 +31,12 @@ export default {
 }
 h1 {
   font-size: 48px;
+}
+ol {
+  width: 600px;
+  margin: auto;
+}
+li {
+  text-align: left;
 }
 </style>
